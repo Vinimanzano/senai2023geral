8c1c996601ce7cfec1240842c0cac2136d602a3e
mirellaxsj <vini.manzano5@gmail.com>
2023-08-07 10:45:47 -0300

    code runner

--- FPOO/Exercicios1Des-2/ex1.c
+++ FPOO/Exercicios1Des-2/ex1.c
@@ -7,9 +7,8 @@ int main(){
 	
 	if(preco > 1000){
 		preco = preco * 0.92;
-		printf("O preco do produto com desconto de 8%% e de R$%.2f", preco);
-	} else{
-		printf("O preco do produto nao sofreu alteracoes.");
 	}
+		printf("O preco do produto com desconto de 8%% e de R$%.2f", preco);
+	
 	return 0;
 }

b2431bc99e72d1d8ecf88dde6f9974798766fa23
Vinícius Manzano <139992933+Vinimanzano@users.noreply.github.com>
2023-08-07 10:38:48 -0300

    Update ex1.c

--- FPOO/Exercicios1Des-2/ex1.c
+++ FPOO/Exercicios1Des-2/ex1.c
@@ -8,8 +8,6 @@ int main(){
 	if(preco > 1000){
 		preco = preco * 0.92;
 		printf("O preco do produto com desconto de 8%% e de R$%.2f", preco);
-	} else{
-		printf("O preco do produto nao sofreu alteracoes.");
 	}
 	return 0;
 }

045d05a80537a9d8685e569d94b12fbc9ef71b05
Vinícius Manzano <139992933+Vinimanzano@users.noreply.github.com>
2023-08-10 11:22:57 -0300

    Update exercicio6.c

--- FPOO/Exercicio1Des-10-8/exercicio6.c
+++ FPOO/Exercicio1Des-10-8/exercicio6.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main(){
-  int i;
-  do{
-  		printf("Digite um numero inteiro:");
-  		scanf("%d",&i);
-  		printf("O quadado È %d\n", i * i);
-  }while(1 != 4);
+	int i = 0;
+	while(i != 4){
+		printf("Digite um n√∫mero inteiro: ");
+		scanf("%d",&i);
+	}
 	return 0;
 }

babd3fa33a53fbf4b96df2e02f41d591056573dc
Vinícius Manzano <139992933+Vinimanzano@users.noreply.github.com>
2023-09-13 10:16:02 -0300

    Update func6.c

--- FPOO/func - aula 12/func6.c
+++ FPOO/func - aula 12/func6.c
@@ -1,9 +1,9 @@
 //função que retorna a média de três parâmetros inteiros
 int media(int a, int b, int c){
-	return (a + b + c) / 2;
+	return (a + b + c) / 3;
 }
 
 int main() {
-	printf("A media de 84 + 16 + 100 / 2 = %d", media(84, 16, 100));
+	printf("A media de 84 + 16 + 100 / 3 = %d", media(84, 16, 100));
 	return 0;
 }

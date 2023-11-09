d1eaf13a3e021ff3b761532d280ceb14bdb70129
JMarques12 <beckham.souza06@eaportal.org>
2023-11-09 10:29:40 -0300

    SENAI

--- Portifolio/assets/js.js
+++ Portifolio/assets/js.js
@@ -33,3 +33,20 @@ e.addEventListener("animationend", listener, false);
 e.addEventListener("animationiteration", listener, false);
 
 e.className = "slidein";
+
+jQuery(document).ready(function() {
+  // Exibe ou oculta o botão
+  jQuery(window).scroll(function() {
+      if (jQuery(this).scrollTop() > 200) {
+          jQuery('.voltar-ao-topo').fadeIn(200);
+      } else {
+          jQuery('.voltar-ao-topo').fadeOut(200);
+      }
+  });
+  
+  // Faz animação para subir
+  jQuery('.voltar-ao-topo').click(function(event) {
+      event.preventDefault();
+      jQuery('html, body').animate({scrollTop: 0}, 300);
+  })
+});

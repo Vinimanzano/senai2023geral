1c84dca47b8ed656e0ab5ede75c9b3ad1a3244fe
JMarques12 <beckham.souza06@eaportal.org>
2023-11-10 09:54:40 -0300

    SENAI

--- Portifolio/assets/js.js
+++ Portifolio/assets/js.js
@@ -1,39 +1,3 @@
-function typeWrite(elemento){
-    const textoArray = elemento.innerHTML.split('');
-    elemento.innerHTML = ' ';
-    textoArray.forEach(function(letra, i){   
-      
-    setTimeout(function(){
-        elemento.innerHTML += letra;
-    }, 75 * i)
-
-  });
-}
-const titulo = document.querySelector('.titulo-principal');
-typeWrite(titulo);
-
-function listener(e) {
-  var l = document.createElement("li");
-  switch (e.type) {
-    case "animationstart":
-      l.innerHTML = "Started: elapsed time is " + e.elapsedTime;
-      break;
-    case "animationend":
-      l.innerHTML = "Ended: elapsed time is " + e.elapsedTime;
-      break;
-    case "animationiteration":
-      l.innerHTML = "New loop started at time " + e.elapsedTime;
-      break;
-  }
-  document.getElementById("output").appendChild(l);
-}
-var e = document.getElementById("watchme");
-e.addEventListener("animationstart", listener, false);
-e.addEventListener("animationend", listener, false);
-e.addEventListener("animationiteration", listener, false);
-
-e.className = "slidein";
-
 jQuery(document).ready(function() {
   // Exibe ou oculta o botão
   jQuery(window).scroll(function() {

cb8002131e27248d2821ba1f4acac9cfc8a932c8
JMarques12 <beckham.souza06@eaportal.org>
2023-11-13 09:23:09 -0300

    SENAI

--- Portifolio/assets/js.js
+++ Portifolio/assets/js.js
@@ -1,16 +1,16 @@
-jQuery(document).ready(function() {
-  // Exibe ou oculta o botão
-  jQuery(window).scroll(function() {
-      if (jQuery(this).scrollTop() > 200) {
-          jQuery('.voltar-ao-topo').fadeIn(200);
-      } else {
-          jQuery('.voltar-ao-topo').fadeOut(200);
-      }
-  });
+// jQuery(document).ready(function() {
+//   // Exibe ou oculta o botão
+//   jQuery(window).scroll(function() {
+//       if (jQuery(this).scrollTop() > 400) {
+//           jQuery('.voltarTopo').fadeIn(400);
+//       } else {
+//           jQuery('.voltarTopo').fadeOut(400);
+//       }
+//   });
   
-  // Faz animação para subir
-  jQuery('.voltar-ao-topo').click(function(event) {
-      event.preventDefault();
-      jQuery('html, body').animate({scrollTop: 0}, 300);
-  })
-});
+//   // Faz animação para subir
+//   jQuery('.voltarTopo').click(function(event) {
+//       event.preventDefault();
+//       jQuery('html, body').animate({scrollTop: 0}, 600);
+//   })
+// });
